0b0de428eb969c59c94e99cd9bef04724aabe927
RaviNair <rnair@mobiquityinc.com>
2019-04-02 11:23:26 +0530

    transaction test file added

---
+++ TransactionTest.java
@@ -0,0 +1,7 @@
+String holdReference = "2018120717540001";
+        String accountId = "861000191011";
+
+        ReleaseHoldRequest releaseHoldRequest = ReleaseHoldRequest.builder()
+                .holdReference(holdReference)
+                .accountId(accountId)
+                .build();
